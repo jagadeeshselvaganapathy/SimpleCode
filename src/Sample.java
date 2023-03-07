1531ced584b1261ded768f920d76558bfdcd25d6
jagadeesh <jagadeeshpragadeesh@gmail.com>
2023-03-07 11:46:57 +0530

    first coding

---
+++ src/Sample.java
@@ -0,0 +1,14 @@
+
+public class Sample {
+	
+	private void emp() {
+
+		System.out.println("yes");
+		
+	}
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.emp();
+	}
+
+}
